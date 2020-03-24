05e465a72002514757d891afe8480e0280b945f7
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2020-03-24 14:18:02 -0600

    Add TMPro and some lines of code

---
+++ Assets/_game/Manager_Situations.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Manager_Situations : MonoBehaviour
+{
+    public TextMeshProUGUI texto;
+    public GameObject Hacha1;
+    public GameObject Hacha2;
+    public GameObject Espada1;
+    public GameObject Espada2;
+    public GameObject Lanza1;
+    public GameObject Lanza2;
+
+    public GameObject[] Activos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Activos.Length > 1)
+        {
+            texto.text = "Estan en escena:\n" + Activos[0].gameObject.GetComponent<ImageTargetController>().TargetName
+                    + "\n y \n" + Activos[1].gameObject.GetComponent<ImageTargetController>().TargetName;
+            if (Vector3.Distance(Activos[0].transform.position, Activos[1].transform.position) < 1.0f)
+            {
+                texto.text = "ESTAN CERCA:\n" + Activos[0].gameObject.GetComponent<ImageTargetController>().TargetName
+                    + "\n y \n" + Activos[1].gameObject.GetComponent<ImageTargetController>().TargetName;
+            }
+        }
+    }
+
+
+}
