4133b2f1d05e4c6a0afad37efb2dc2113cb0e9b1
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2020-03-31 13:34:07 -0600

    Solo una particula

--- Assets/_game/Comunicadores.cs
+++ Assets/_game/Comunicadores.cs
@@ -17,6 +17,7 @@ public class Comunicadores : MonoBehaviour
     public float disapear;
     public Material mt;
     public ParticleSystem ps;
+    bool doOnce = true;
 
     private void Start()
     {
@@ -26,19 +27,23 @@ public class Comunicadores : MonoBehaviour
 
     private void OnEnable()
     {
-        DOTween.To(() => disapear, x => disapear = x, 1, 10.0f).OnUpdate(() => { mt.SetFloat("_Noise_Slider", disapear); });
         ms.AddActivo(gameObject);
     }
 
     private void OnDisable()
     {
         ms.RemoveActivo(gameObject);
+        doOnce = true;
     }
 
     public void Disapear()
     {
         disapear = 0;
-        ps.Play();
+        if (doOnce)
+        {
+            doOnce = false;
+            ps.Play();
+        }
         DOTween.To(() => disapear, x => disapear = x, 1, 2.0f).OnUpdate(() => { mt.SetFloat("_Noise_Slider", disapear); });
     }
 }
