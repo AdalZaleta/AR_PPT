01c45332b9da11eb6020ff2b712609c7a5b283b5
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2020-03-31 13:28:21 -0600

    Cosas pro

--- Assets/_game/Comunicadores.cs
+++ Assets/_game/Comunicadores.cs
@@ -16,6 +16,7 @@ public class Comunicadores : MonoBehaviour
     public typeWeapon type;
     public float disapear;
     public Material mt;
+    public ParticleSystem ps;
 
     private void Start()
     {
@@ -25,7 +26,7 @@ public class Comunicadores : MonoBehaviour
 
     private void OnEnable()
     {
-        DOTween.To(() => disapear, x => disapear = x, 1, 2.0f).OnUpdate(() => { mt.SetFloat("_Noise_Slider", disapear); });
+        DOTween.To(() => disapear, x => disapear = x, 1, 10.0f).OnUpdate(() => { mt.SetFloat("_Noise_Slider", disapear); });
         ms.AddActivo(gameObject);
     }
 
@@ -37,6 +38,7 @@ public class Comunicadores : MonoBehaviour
     public void Disapear()
     {
         disapear = 0;
+        ps.Play();
         DOTween.To(() => disapear, x => disapear = x, 1, 2.0f).OnUpdate(() => { mt.SetFloat("_Noise_Slider", disapear); });
     }
 }
